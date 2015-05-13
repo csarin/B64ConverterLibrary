aadba650f936b35b7a4c634660a041568abdec3d
csarin <me@cesarreneses.net>
2015-05-13 13:34:14 +0200

    Base64 converter first version

---
+++ Base64ImageConverter/Helpers/B64ConverterLibrary.cs
@@ -0,0 +1,59 @@
+﻿//----------------------------------------------------------------------------------//
+//
+//<author> César Reneses Cárcamo </author>
+//<date> 13/05/2015  </date>
+//<summary> Class helper convert Base64 to Image and a Image to Base64 </summary>
+//<website> http://cesarreneses.net </website>
+//
+//----------------------------------------------------------------------------------//
+
+namespace Base64ImageConverter.Helpers
+{
+    #region Usings
+
+    using System;
+    using System.IO;
+    using System.Drawing.Imaging;
+
+    #endregion
+
+    public static class B64ConverterLibrary
+    {
+        #region Public Methods
+
+        /// <summary>
+        /// Convert Image to Base64 string.
+        /// </summary>
+        /// <param name="img"> Image source</param>
+        /// <param name="format">Jpeg, Png, Tif, etc</param>
+        /// <returns>Base64 string</returns>
+        public static string ImageToBase64(System.Drawing.Image img, ImageFormat format)
+        {
+            using (MemoryStream ms = new MemoryStream())
+            {
+                img.Save(ms, format);
+                byte[] imgBytes = ms.ToArray();
+
+                string base64String = Convert.ToBase64String(imgBytes);
+                return base64String;
+            }
+        }
+
+        /// <summary>
+        /// Convert Base64 string to Image.
+        /// </summary>
+        /// <param name="base64String">Base64 string</param>
+        /// <returns>Object System.Drawing.Image</returns>
+        public static System.Drawing.Image Base64ToImage(string base64String)
+        {
+                byte[] imgBytes = Convert.FromBase64String(base64String);
+                MemoryStream ms = new MemoryStream(imgBytes, 0, imgBytes.Length);
+
+                ms.Write(imgBytes, 0, imgBytes.Length);
+                System.Drawing.Image image = System.Drawing.Image.FromStream(ms, true);
+                return image;
+        }
+
+        #endregion
+    }
+}
---
+++ Base64ImageConverter/MainWindow.xaml.cs
@@ -0,0 +1,61 @@
+﻿//----------------------------------------------------------------------------------//
+//
+//<author> César Reneses Cárcamo </author>
+//<date> 13/05/2015  </date>
+//<summary> Class helper convert Base64 to Image and a Image to Base64 </summary>
+//<website> http://cesarreneses.net </website>
+//
+//----------------------------------------------------------------------------------//
+
+namespace Base64ImageConverter
+{
+    #region Usings
+
+    using Base64ImageConverter.Helpers;
+    using Microsoft.Win32;
+    using System.Drawing.Imaging;
+    using System.IO;
+    using System.Windows;
+
+    #endregion
+
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btnSelectImage_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            if (openFileDialog.ShowDialog() == true)
+                txbSourcePath.Text = openFileDialog.FileName;
+        }
+
+        private void btnConvertToBase64_Click(object sender, RoutedEventArgs e)
+        {
+            System.Drawing.Image img = System.Drawing.Image.FromFile(txbSourcePath.Text);
+
+            txtBase64.Text = B64ConverterLibrary.ImageToBase64(img, ImageFormat.Jpeg);
+        }
+
+        private void btnConvertToImage_Click(object sender, RoutedEventArgs e)
+        {
+            var image = B64ConverterLibrary.Base64ToImage(txtBase64.Text);
+
+            var bitmap = new System.Windows.Media.Imaging.BitmapImage();
+            bitmap.BeginInit();
+            MemoryStream memoryStream = new MemoryStream();
+            image.Save(memoryStream, ImageFormat.Bmp);
+            memoryStream.Seek(0, System.IO.SeekOrigin.Begin);
+            bitmap.StreamSource = memoryStream;
+            bitmap.EndInit();
+
+            ImgConverted.Source = bitmap;
+        }
+    }
+}
